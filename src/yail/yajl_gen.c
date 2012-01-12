d0562c79994ba410d07eb5bfe43ab21c06eed143
lord-kefir <lord-kefir@arcor.de>
2012-01-12 07:01:35 +0100

    removed some unrequired delarations from yail_gen.c

--- src/yail/yajl_gen.c
+++ src/yail/yajl_gen.c
@@ -209,12 +209,6 @@ yajl_gen_integer(yajl_gen g, long long int number)
     return yajl_gen_status_ok;
 }
 
-#if defined(_WIN32) || defined(WIN32)
-#include <float.h>
-#define isnan _isnan
-#define isinf !_finite
-#endif
-
 yajl_gen_status
 yajl_gen_double(yajl_gen g, double number)
 {
